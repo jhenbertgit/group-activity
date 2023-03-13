cb9acc7f2063f912510339fc118a1637b45c44be
Jhenbert Villamucho <jvv205756@gmail.com>
2023-03-14 00:39:32 +0800

    solution

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 const api_url =
   "https://raw.githubusercontent.com/kronusme/dota2-api/master/data/heroes.json";
 
-const getData = async () => {
+//fetching data
+const getJson = async () => {
   try {
     const response = await fetch(api_url);
+    if (!response.ok) {
+      throw new Error("Server failed to fetch");
+    }
     const data = await response.json();
-
-    for (let [key, val] of Object.entries(data)) {
-      console.log(val);
+    for (let val of Object.entries(data)) {
+      return val;
     }
   } catch (err) {
     console.error(err);
   }
 };
 
-getData();
+//display as JSON object
+getJson().then((data) => {
+  const jsonDisplay = JSON.stringify(data);
+  document.getElementById("jsonData").innerText = jsonDisplay;
+});
+
+//display tubular format
+getJson().then((data) => {
+  let tblDatArr = new Array();
+  for (let val of data[1]) {
+    const { id, name, localized_name } = val;
+
+    const row = document.createElement("tr");
+
+    tblDatArr = [id, name, localized_name];
+    for (let i = 0; i < tblDatArr.length; i++) {
+      const td = document.createElement("td");
+      const cellText = document.createTextNode(tblDatArr[i]);
+      td.appendChild(cellText);
+      row.appendChild(td);
+    }
+    document.querySelector("tbody").appendChild(row);
+  }
+});
